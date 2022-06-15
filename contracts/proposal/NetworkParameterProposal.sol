474676568232786506d9d763f0a741350f85f18c
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2022-06-15 13:48:43 +0400

    Use Address to reference SFC

--- contracts/proposal/NetworkParameterProposal.sol
+++ contracts/proposal/NetworkParameterProposal.sol
@@ -11,7 +11,7 @@ interface SFC {
  * @dev NetworkParameter proposal
  */
 contract NetworkParameterProposal is DelegatecallExecutableProposal, Cancelable {
-    SFC public sfc;
+    address public sfc;
 
     constructor(string memory __name, string memory __description, bytes32[] memory __options, 
         uint256 __minVotes, uint256 __minAgreement, uint256 __start, uint256 __minEnd, uint256 __maxEnd,
@@ -25,7 +25,7 @@ contract NetworkParameterProposal is DelegatecallExecutableProposal, Cancelable 
         _start = __start;
         _minEnd = __minEnd;
         _maxEnd = __maxEnd;
-        sfc = SFC(_sfc);
+        sfc = _sfc;
         // verify the proposal right away to avoid deploying a wrong proposal
         if (verifier != address(0)) {
             require(verifyProposalParams(verifier), "failed verification");
@@ -36,7 +36,7 @@ contract NetworkParameterProposal is DelegatecallExecutableProposal, Cancelable 
 
     function execute_delegatecall(address selfAddr, uint256 newValue) external {
         NetworkParameterProposal self = NetworkParameterProposal(selfAddr);
-        (SFC(self.sfc())).setMaxDelegation(100);
+        SFC(self.sfc()).setMaxDelegation(100);
         emit NetworkParameterUpgradeIsDone(newValue);
     }
 }
